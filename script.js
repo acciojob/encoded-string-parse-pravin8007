92fdedecb49b42e4249c5327217f0396608360e0
Pravin Patil <106026547+pravin8007@users.noreply.github.com>
2023-12-19 12:04:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const parseCode = (str) => {
   // your code here
+let arr = [firstName, lastName, id] ;
+ arr = encodedString.split("000");
+	let ans =  {
+    firstName: firstName,
+    lastName: lastName,
+    id: id
+  };
+	return ans ;
 };
 
 // Do not change the code below
